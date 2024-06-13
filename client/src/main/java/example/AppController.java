84f676b761d927197d8e14abe5e3d02892750563
CORP\phmontei <philippe.a.monteiro@sogeti.com>
2024-06-13 15:04:41 +0200

    test avec un autre fichier de config de profil

--- client/src/main/java/example/AppController.java
+++ client/src/main/java/example/AppController.java
@@ -1,5 +1,6 @@
 package example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class AppController {
 
+    @Autowired
     private final ApplicationPropertiesConfiguration appProperties;
 
 //    @Value("${info.foo}")
