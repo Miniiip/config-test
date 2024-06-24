0ed7ac3c33189e9458b36e506ecfcfa4994cea0a
CORP\phmontei <philippe.a.monteiro@sogeti.com>
2024-06-24 10:57:46 +0200

    maj pour tester tag

--- client/src/main/java/example/AppController.java
+++ client/src/main/java/example/AppController.java
@@ -1,28 +1,16 @@
 package example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// This will allow us to reinitialize this controller to get any new config
-// values when the /refresh endpoint is POSTed to.
 @RefreshScope
 public class AppController {
 
     @Autowired
     private final ApplicationPropertiesConfiguration appProperties;
-
-//    @Value("${info.foo}")
-//    private String fooProperty;
-
-//    @Value("${dev.message}")
-//    private String messageDev;
-//
-//    @Value("${prod.message}")
-//    private String messageProd;
 
     public AppController(ApplicationPropertiesConfiguration appProperties) {
         this.appProperties = appProperties;
@@ -30,21 +18,7 @@ public class AppController {
 
     @RequestMapping("/")
     public String hello() {
-        return "On utilise [" + appProperties.getFoo() + "] depuis le config server";
+        return "On utilise [" + appProperties.getFoo() + "] depuis le config server sur le client";
     }
-
-//    @RequestMapping("/someList")
-//    public String someList() {
-//        return "Voici la liste " + appProperties.getSomeList().toString() ;
-//    }
-
-
-//    @RequestMapping("/dev")
-//    public String helloDev() {
-//        return messageDev;
-//    }
-//
-//    @RequestMapping("/prod")
-//    public String helloProd() {return messageProd;}
 
 }
--- client/src/main/java/example/ApplicationPropertiesConfiguration.java
+++ client/src/main/java/example/ApplicationPropertiesConfiguration.java
@@ -1,35 +1,17 @@
 package example;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Getter
+@Setter
 @Component
 @ConfigurationProperties("client")
 @RefreshScope
 public class ApplicationPropertiesConfiguration
 {
     private String foo;
-
-//    private List<Long> someList;
-
-    public String getFoo()
-    {
-        return foo;
-    }
-
-    public void setFoo(String foo)
-    {
-        this.foo = foo;
-    }
-
-//    public List<Long> getSomeList() {
-//        return someList;
-//    }
-//
-//    public void setSomeList(List<Long> someList) {
-//        this.someList = someList;
-//    }
 }
